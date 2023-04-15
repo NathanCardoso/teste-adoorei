295e1bca0c5e760f2bab32a3f61da88c16f85020
NathanCardoso <nathan.cardoso@codezone.com.br>
2023-04-14 21:02:19 -0300

    feat[InputForm]: create new component

--- src/components/UserLogin.vue
+++ src/components/UserLogin.vue
@@ -1,27 +1,34 @@
 <template>
-	<form>
-		<TitleContent content="Entre na sua conta"/>
-		<ParagraphContent content="Para acessar sua conta informe seu e-mail e senha"/>
-	</form>
+  <section class="user-login">
+    <div class="container">
+      <form>
+        <TitleContent content="Entre na sua conta" />
+        <ParagraphContent
+          content="Para acessar sua conta informe seu e-mail e senha"
+        />
+      </form>
+    </div>
+  </section>
 </template>
 
 <script>
-import ParagraphContent from './atoms/ParagraphContent.vue'
-import TitleContent from './atoms/TitleContent.vue'
+import ParagraphContent from "./atoms/ParagraphContent.vue";
+import TitleContent from "./atoms/TitleContent.vue";
 
 export default {
-	name: 'UserLogin',
-	data() {
-		return {
-
-		}
-	},
-	components: {
-		TitleContent,
-		ParagraphContent
-	}
-}
+  name: "UserLogin",
+  data() {
+    return {};
+  },
+  components: {
+    TitleContent,
+    ParagraphContent,
+  },
+};
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+.user-login {
+  margin: 0 auto;
+}
 </style>
---
+++ src/components/atoms/InputForm.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <label :for="inputId">{{ labelContent }}</label>
+    <input
+      :type="inputType"
+      :id="inputId"
+      :name="inputName"
+      :placeholder="inputPlaceholder"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InputForm",
+  props: {
+    labelContent: {
+      type: String,
+      default: "",
+      required: true,
+    },
+    inputId: {
+      type: String,
+      default: "",
+      required: true,
+    },
+    inputType: {
+      type: String,
+      default: "",
+      required: true,
+    },
+    inputName: {
+      type: String,
+      default: "",
+      required: true,
+    },
+    inputPlaceholder: {
+      type: String,
+      default: "",
+      required: true,
+    },
+  },
+  data() {
+    return {
+      inputId: this.inputId,
+			inputName: this.inputName,
+			inputType: this.inputType,
+			inputPlaceholder: this.inputPlaceholder,
+			labelContent: this.labelContent
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
