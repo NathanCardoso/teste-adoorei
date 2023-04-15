e50c9b8dfee3ec1bcaa77aa6e7eea0b3c00e20d4
NathanCardoso <nathan.cardoso@codezone.com.br>
2023-04-14 23:10:08 -0300

    feat [App]: to add style

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,32 @@
 <template>
-	<LogoLocaWeb />
-	<UserLogin/>
+  <main class="main">
+    <LogoLocaWeb />
+    <UserLogin />
+  </main>
 </template>
 
 <script>
-import LogoLocaWeb from "./components/atoms/LogoLocaWeb.vue"
-import  UserLogin from './components/UserLogin.vue'
+import LogoLocaWeb from "./components/atoms/LogoLocaWeb.vue";
+import UserLogin from "./components/UserLogin.vue";
 
 export default {
-	data() {
-		return {}
-	},
-	components: {
-		UserLogin,
-		LogoLocaWeb
-	}
-}
+  data() {
+    return {};
+  },
+  components: {
+    UserLogin,
+    LogoLocaWeb,
+  },
+};
 </script>
 
 <style lang="scss">
-	
+.main {
+	height: 100vh;
+  display: flex;
+  justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	gap: rem(40)
+}
 </style>
