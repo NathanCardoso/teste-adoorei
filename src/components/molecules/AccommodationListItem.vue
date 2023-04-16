553fd39e92c77ff4a6c42b449b3dba8827fc4400
NathanCardoso <nathan.cardoso@codezone.com.br>
2023-04-16 13:53:49 -0300

    feat[AccommodationList]: create new component

---
+++ src/components/molecules/AccommodationListItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <li class="accommodation-list-item">
+    <div class="item-image">
+      <img class="image" src="../../assets/image/check.svg" alt="Check" />
+    </div>
+    <ParagraphContent />
+  </li>
+</template>
+
+<script>
+import ParagraphContent from "../atoms/ParagraphContent.vue";
+export default {
+  name: "AccommodationListItem",
+  components: {
+    ParagraphContent,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+	.accommodation-list-item {
+		display: flex;
+		gap: rem(16);
+		align-items: center;
+		justify-content: center;
+
+		.item-image {
+			.image {
+				display: block;
+				width: 100%;
+			}
+		}
+	}
+</style>
