bdd6fca0c5b7be003a518da6d9091c614fc29aed
NathanCardoso <nathan.cardoso@codezone.com.br>
2023-04-16 13:07:24 -0300

    feat[AccommodationServer]: create new component

---
+++ src/components/AccommodationServer.vue
@@ -0,0 +1,29 @@
+<template>
+	<article class="accommodation-server">
+		<AccommodationNumber content="Hospedagem 1"/>
+		<AccommodationPrice price="Grátis"/>
+		<AccommodationDetails content="Você não paga nada para usar sem taxa de setup"/>
+	</article>	
+</template>
+
+<script>
+import AccommodationNumber from "./atoms/AccommodationNumber.vue"
+import AccommodationPrice from "./atoms/AccommodationPrice.vue"
+import AccommodationDetails from "./atoms/AccommodationDetails.vue"
+
+export default {
+	name: "AccommodationServer",
+	components: {
+		AccommodationNumber,
+		AccommodationPrice,
+		AccommodationDetails
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+	.accommodation-server {
+		background: $white;
+		padding: rem(30) rem(24);
+	}
+</style>
