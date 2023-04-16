49a9aa7dae4c51e360e329bff28dee3ff2991e94
NathanCardoso <nathan.cardoso@codezone.com.br>
2023-04-16 15:08:19 -0300

    feat[ListTitle]: create new component

---
+++ src/components/atoms/ListTitle.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="accommodation-title">
+    <h3 class="title">
+      {{ titleContent }} <span v-if="serverCountry" class="server-country">{{ serverCountry }}</span>
+    </h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AccommodationTitle",
+  props: {
+    titleContent: {
+      type: String,
+      default: "",
+      required: true,
+    },
+    serverCountry: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      titleContent: this.titleContent,
+      serverCountry: this.serverCountry,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+	.accommodation-title {
+		.title {
+			color: $black;
+			font-size: rem(20);
+
+			.server-country {
+				text-decoration: underline;
+			}
+		}
+	}
+</style>
