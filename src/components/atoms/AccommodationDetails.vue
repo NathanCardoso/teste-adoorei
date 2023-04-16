3567958dc00679dd811f28e27a9712ae295db73e
NathanCardoso <nathan.cardoso@codezone.com.br>
2023-04-16 13:09:15 -0300

    feat[AccommodationDetails]: create new component

---
+++ src/components/atoms/AccommodationDetails.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="accommodation-details">
+		<p class="details">{{content}}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "AccommodationDetails",
+	props: {
+		content: {
+			type: String,
+			default: "",
+			required: true
+		}
+	},
+	data() {
+		return {
+			content: this.content
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+	.accommodation-details {
+		display: flex;
+		justify-content: center;
+
+		.details {
+			color: $black;
+			font-size: rem(16);
+			line-height: rem(22);
+		}
+	}
+</style>
