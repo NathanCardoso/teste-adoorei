91c30262e0f67026a6bb070c5c670b39538727ef
NathanCardoso <nathan.cardoso@codezone.com.br>
2023-04-16 13:08:31 -0300

    feat[AccommodationPrice]: create new component

---
+++ src/components/atoms/AccommodationPrice.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="accommodation-price">
+		<p class="price">{{price}}</p>
+	</div>
+</template>
+
+<script>
+export default {
+  name: "AccommodationPrice",
+	props: {
+		price: {
+			type: String,
+			default: ""
+		}
+	},
+	data() {
+		return {
+			price: this.price
+		}
+	}
+};
+</script>
+
+<style lang="scss">
+	.accommodation-price {
+		display: flex;
+		justify-content: center;
+
+		.price {
+			color: $pink;
+			font-size: rem(24);
+		}
+	}
+</style>
